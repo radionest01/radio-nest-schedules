9e6bde1544372f1d90189dc086da984cdef78993
radionest01 <radionest1@gmail.com>
2026-02-02 04:37:49 +0000

    Update firebase.js

--- admin/firebase.js
+++ admin/firebase.js
@@ -1,1 +1,17 @@
+// Your Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBYX0zC6sNg02nhyDp36oPi-XOHQkOniro",
+  authDomain: "radio-nest-schedules.firebaseapp.com",
+  projectId: "radio-nest-schedules",
+  storageBucket: "radio-nest-schedules.firebasestorage.app",
+  messagingSenderId: "461314444975",
+  appId: "1:461314444975:web:4872e913324e789b6a4e5a"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Firebase services
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
